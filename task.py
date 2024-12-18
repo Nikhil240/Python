51db99430e2a7899980a579e2514ea2bd38c2d3f
Nikhil240 <69969844+Nikhil240@users.noreply.github.com>
2024-12-18 13:44:25 -0600

    Update task.py

--- task.py
+++ task.py
@@ -3,3 +3,5 @@ city = input("Enter the name of the city you were born in?\n")
 pet = input ("Enter the name of your fisrt pet?\n")
 print("Your band name could be " + city +" "+ pet)
 
+#This code is used to take input from the user and then create a band name
+#It uses input function, print fucntion, variables, and string concatenation
